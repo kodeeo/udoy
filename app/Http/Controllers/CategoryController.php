b18ffb5a7d88d7b051807c569d74bf90daafcc0b
unknown <afrinnishe99@gmail.com>
2022-03-23 17:12:13 +0600

    crud for category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories=Category::all();
+        return view('admin.pages.categories.list',compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.pages.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+
+            'category_name'=>'required',
+            'category_details'=>'required',
+        ]);
+
+        Category::create([
+            'name'=>$request->category_name,
+            'details'=>$request->category_details,
+          
+           ]);
+
+           return redirect()->back()->with('success','Add Category Successfully');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $edit_category=Category::find($id);
+        return view('admin.pages.categories.edit',compact('edit_category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $edit_category=Category::find($id);
+        $edit_category->update([
+            'name'=>$request->category_name,
+            'details'=>$request->category_details,
+        ]);
+        return redirect()->back()->with('success','Update Category Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $categories=Category::find($id)->delete();
+     return redirect()->back()->with('success','Category Deleted');
+    }
+}
