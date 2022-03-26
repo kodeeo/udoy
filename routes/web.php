edc92d70199e0463d365f1997a9e5ec4b5f9a3ff
Miftahur23 <miftahur109970@gmail.com>
2022-03-26 14:42:32 +0600

    customer_crud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,19 @@ Route::get('/', function () {
     return view('admin.master');
 });
 
+//cateogory
 Route::get('/add/category/',[CategoryController::class,'create'])->name('category.add');
 Route::get('/list/category/',[CategoryController::class,'index'])->name('category.index');
 Route::post('/store/category/',[CategoryController::class,'store'])->name('category.store');
 Route::get('/edit/category/{id}',[CategoryController::class,'edit'])->name('category.edit');
 Route::put('/update/category/{id}',[CategoryController::class,'update'])->name('category.update');
 Route::get('/delete/category/{id}',[CategoryController::class,'destroy'])->name('category.delete');
+
+//customers
+Route::get('/list/customers/',[CustomerController::class,'index'])->name('customers.index');
+Route::get('/add/customers/',[CustomerController::class,'create'])->name('customers.create');
+Route::post('/store/customers/',[CustomerController::class,'store'])->name('customers.store');
+Route::get('/show/customers/{id}',[CustomerController::class,'show'])->name('customers.show');
+Route::get('/edit/customers/{id}',[CustomerController::class,'edit'])->name('customers.edit');
+Route::put('/update/customers/{id}',[CustomerController::class,'update'])->name('customers.update');
+Route::get('/delete/customers/{id}',[CustomerController::class,'destroy'])->name('customers.delete');
